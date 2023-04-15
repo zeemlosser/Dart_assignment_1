4b6bcb4f49bf984d3431c8a78b358918f1f4b9ca
zeemlooser <myhpc0153@gmail.com>
2023-04-15 23:06:23 +0600

    Update the Assignment

--- bin/assignmnet_1.dart
+++ bin/assignmnet_1.dart
@@ -1,6 +1,14 @@
 void main(){
  List<String> phoneNumber = [ '+88', '01768131685', '01768171985', '01768111286', '01768131685'];
- bool Remove= phoneNumber.remove('+88');
- print(phoneNumber);
+ String one = phoneNumber[0] + phoneNumber[1];
+ String two = phoneNumber[0] + phoneNumber[2];
+ String three = phoneNumber[0] + phoneNumber[3];
+ String four = phoneNumber[0] + phoneNumber[4];
+
+
+ print(one);
+ print(two);
+ print(three);
+ print(four);
 
 }
